56204342a82de4dd2535e174df831a03540bef99
Srikar Merugu <srikar@foodbridge.ai>
2026-03-19 00:27:29 +0530

    fix: whitelist i.pravatar.cc and cloudinary image hostnames

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: false,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.pravatar.cc',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            }
+        ],
+    },
 };
 
 module.exports = nextConfig;
